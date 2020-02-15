608283669a84b3e3a7c6a3c29b750339af844c5e
Brian Daley <Brian_Daley@keybank.com>
2020-02-15 16:15:16 -0500

    Master-updated-to-the-latest-plugins implemented breaking changes

--- lib/app/home/entries/entry_job.dart
+++ lib/app/home/entries/entry_job.dart
@@ -4,6 +4,11 @@ import 'package:time_tracker_flutter_course/app/home/models/job.dart';
 class EntryJob {
   EntryJob(this.entry, this.job);
 
+  @override
+  String toString() {
+    return 'EntryJob{entry: $entry, job: $job}';
+  }
+
   final Entry entry;
   final Job job;
 }
